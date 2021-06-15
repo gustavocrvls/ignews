2f95a060a066e285e7a7c564df911fca7b6ea23a
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-06-14 22:18:50 -0300

    Fix the load twice bug

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -34,17 +34,19 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const session = await getSession({ req });
+  // const session = await getSession({ req });
   const { slug } = params;
 
-  if (!session || !session.activeSubscription) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      }
-    }
-  }
+  console.log(slug)
+
+  // if (!session || !session.activeSubscription) {
+  //   return {
+  //     redirect: {
+  //       destination: '/',
+  //       permanent: false,
+  //     }
+  //   }
+  // }
 
   const prismic = getPrismicClient(req);
 
